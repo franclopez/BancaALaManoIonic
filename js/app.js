a5e6b9ad9528249f83c96b1c9a2e586dccb82891
Francisco Lopez <franclopez@gmail.com>
2014-03-16 16:33:20 -0500

    Adicion de fuentes. Pagina de consulta de saldo y ajuste de navegacion

--- js/app.js
+++ js/app.js
@@ -6,9 +6,7 @@
 // 'bancaalamano.services' is found in services.js
 // 'bancaalamano.controllers' is found in controllers.js
 angular.module('bancaalamano', ['ionic', 'bancaalamano.services', 'bancaalamano.controllers'])
-
-
-.config(function($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -23,7 +21,6 @@ angular.module('bancaalamano', ['ionic', 'bancaalamano.services', 'bancaalamano.
       templateUrl: "templates/tabs.html"
     })
 
-    // the pet tab has its own child nav-view and history
     .state('tab.bancaalamano-index', {
       url: '/bancaalamano',
       views: {
@@ -38,6 +35,15 @@ angular.module('bancaalamano', ['ionic', 'bancaalamano.services', 'bancaalamano.
       views: {
         'transactions-tab': {
           templateUrl: 'templates/transactions.html'
+        }
+      }
+    })
+
+    .state('tab.balance', {
+      url: '/balance',
+      views: {
+        'transactions-tab': {
+          templateUrl: 'templates/balance-inquiry.html'
         }
       }
     })
