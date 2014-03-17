5eba89b93508b90fe6550135a73cd5e78ab396dd
Francisco Lopez <franclopez@gmail.com>
2014-03-17 17:50:57 -0500

    Ajsute pagina logon

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,23 @@
-angular.module('bancaalamano.controllers', [])
+angular.module('bancaalamano.controllers', ['ionic'])
 	
 	.controller('SignInCtrl', function($scope, $state) {
 		  $scope.signIn = function(user) {
 		    console.log('Sign-In', user);
-		    $state.go('tab.transactions');
+		    if(user != undefined) {
+		    	$state.go('tab.transactions');	
+		    }
+		    else {
+		    	alert('Por Favor Ingrese su Usuario y Contraseña');
+		    	/*
+		    	$ionicPopup.alert({
+	              title: 'Datos Incompletos',
+	              content: 'Por Favor Ingrese su Usuario y Contraseña'
+	            }).then(function(res) {
+	              console.log('No ingresó datos de login');
+	            });
+*/
+		    }
+		    
 		  };
 	})
 ;
